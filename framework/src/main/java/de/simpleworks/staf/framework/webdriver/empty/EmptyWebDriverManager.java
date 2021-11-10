085c684d49c8592d5f01f192587ce80bf0b4089a
Marcin Brzoza <marcin.brzoza@simpleworks.de>
2021-11-10 10:38:33 +0100

    Implementation of EmptyDriver.

---
+++ framework/src/main/java/de/simpleworks/staf/framework/webdriver/empty/EmptyWebDriverManager.java
@@ -0,0 +1,15 @@
+package de.simpleworks.staf.framework.webdriver.empty;
+
+import org.openqa.selenium.WebDriver;
+
+import de.simpleworks.staf.framework.gui.webdriver.module.WebDriverManagerImpl;
+
+public class EmptyWebDriverManager extends WebDriverManagerImpl {
+
+	@Override
+	protected WebDriver createDriver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
