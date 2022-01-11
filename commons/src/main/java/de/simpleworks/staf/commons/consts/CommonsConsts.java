6fde8bf21efad2e335153a8e746e752f9345a355
dustin.baer <dustin.baer@simpleworks.de>
2022-01-11 13:53:03 +0100

    add implementation of feature/staf/issues/39

--- commons/src/main/java/de/simpleworks/staf/commons/consts/CommonsConsts.java
+++ commons/src/main/java/de/simpleworks/staf/commons/consts/CommonsConsts.java
@@ -1,7 +1,9 @@
 package de.simpleworks.staf.commons.consts;
 
-//TODO: make this configurable via config files
+
 public class CommonsConsts {
 	public final static String DATE_TIME_FORMATTER = "dd.MM.YYYY-HH:mm:ss";
+	
+	public static final String API_TIMEOUT = "api.timeout";
 	public static final String GUI_TIMEOUT = "gui.timeout";
 }
