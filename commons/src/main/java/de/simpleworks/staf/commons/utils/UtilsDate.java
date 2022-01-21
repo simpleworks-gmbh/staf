65cc93f80f43f3dd168a3c885be354ba0e891756
dustin.baer <dustin.baer@simpleworks.de>
2022-01-21 17:21:23 +0100

    add implementation of bugfix/staf/issues/67

--- commons/src/main/java/de/simpleworks/staf/commons/utils/UtilsDate.java
+++ commons/src/main/java/de/simpleworks/staf/commons/utils/UtilsDate.java
@@ -1,66 +1,51 @@
 package de.simpleworks.staf.commons.utils;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import de.simpleworks.staf.commons.consts.CommonsConsts;
-
 public class UtilsDate {
-	private static final Logger logger = LogManager.getLogger(UtilsDate.class);
-
-	private UtilsDate() {
-		throw new IllegalStateException("utility class.");
-	}
-
-	public static Date getCurrentDateTime() {
-		return Calendar.getInstance().getTime();
-	}
-
-	public static String getCurrentTime() {
-		return UtilsDate.getCurrentTimeFormatted(UtilsDate.getCurrentDateTime());
-	}
-
-	public static String getCurrentTimeFormatted(final Date date) {
-		if (date == null) {
-			throw new IllegalArgumentException("date can't be null.");
-		}
-
-		final String result = new SimpleDateFormat(CommonsConsts.DATE_TIME_FORMATTER).format(date);
-
-		if (UtilsDate.logger.isDebugEnabled()) {
-			UtilsDate.logger.debug(String.format("Return current TimeStamp: '%s'.", result));
-		}
-
-		return result;
-	}
-
-	public static String getCurrentTimeFormatted(final Date date, final SimpleDateFormat sdf) {
-		if (date == null) {
-			throw new IllegalArgumentException("date can't be null.");
-		}
-
-		if (sdf == null) {
-			throw new IllegalArgumentException("sdf can't be null.");
-		}
-
-		final String result = sdf.format(date);
-
-		if (UtilsDate.logger.isDebugEnabled()) {
-			UtilsDate.logger.debug(String.format("Return current TimeStamp: '%s'.", result));
-		}
-
-		return result;
-	}
-
-	public static long getCurrentTimeInMilliSeonds(final Date date) {
-		if (date == null) {
-			throw new IllegalArgumentException("date can't be null.");
-		}
-
-		return date.getTime();
-	}
+    private static final Logger logger = LogManager.getLogger(UtilsDate.class);
+    private UtilsDate() {
+        throw new IllegalStateException("utility class.");
+    }
+    public static Date getCurrentDateTime() {
+        return Calendar.getInstance().getTime();
+    }
+    public static String getCurrentTimestamp() {
+        return Long.toString((new Date()).getTime());
+    }
+    public static String getCurrentTime() {
+        return UtilsDate.getCurrentTimeFormatted(UtilsDate.getCurrentDateTime());
+    }
+    public static String getCurrentTimeFormatted(final Date date) {
+        if (date == null) {
+            throw new IllegalArgumentException("date can't be null.");
+        }
+        final String result = new SimpleDateFormat(CommonsConsts.DATE_TIME_FORMATTER).format(date);
+        if (UtilsDate.logger.isDebugEnabled()) {
+            UtilsDate.logger.debug(String.format("Return current TimeStamp: '%s'.", result));
+        }
+        return result;
+    }
+    public static String getCurrentTimeFormatted(final Date date, final SimpleDateFormat sdf) {
+        if (date == null) {
+            throw new IllegalArgumentException("date can't be null.");
+        }
+        if (sdf == null) {
+            throw new IllegalArgumentException("sdf can't be null.");
+        }
+        final String result = sdf.format(date);
+        if (UtilsDate.logger.isDebugEnabled()) {
+            UtilsDate.logger.debug(String.format("Return current TimeStamp: '%s'.", result));
+        }
+        return result;
+    }
+    public static long getCurrentTimeInMilliSeonds(final Date date) {
+        if (date == null) {
+            throw new IllegalArgumentException("date can't be null.");
+        }
+        return date.getTime();
+    }
 }
--- stafutils-maven-plugin/src/main/java/de/simpleworks/staf/plugin/maven/stafutils/mojo/ArtefactExtractor.java
+++ stafutils-maven-plugin/src/main/java/de/simpleworks/staf/plugin/maven/stafutils/mojo/ArtefactExtractor.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.maven.plugin.AbstractMojo;
@@ -15,7 +14,6 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
 import de.simpleworks.staf.commons.mapper.report.MapperTestcaseReport;
 import de.simpleworks.staf.commons.report.StepReport;
 import de.simpleworks.staf.commons.report.TestcaseReport;
@@ -26,34 +24,25 @@ import de.simpleworks.staf.commons.utils.UtilsIO;
 
 @Mojo(name = "extractArtefacts", defaultPhase = LifecyclePhase.INITIALIZE, threadSafe = true)
 public class ArtefactExtractor extends AbstractMojo {
-
 	private final static Logger logger = LogManager.getLogger(ArtefactExtractor.class);
 	private final static MapperTestcaseReport mapper = new MapperTestcaseReport();
-
 	@Parameter(property = "result", required = true)
 	private String result;
-
 	@Parameter(property = "targetDirectory", required = true)
 	private String targetDirectory;
-
 	private List<TestcaseReport> reports;
 
 	private void init() throws Exception {
-
 		final File resultFile = new File(result);
-
 		if (!resultFile.exists()) {
 			throw new IllegalArgumentException(
 					String.format("resultFile at \"%s\" does not exist.", resultFile.getAbsolutePath()));
 		}
-
 		reports = ArtefactExtractor.mapper.readAll(resultFile).stream().filter(Objects::nonNull)
 				.collect(Collectors.toList());
-
 		if (Convert.isEmpty(reports)) {
 			throw new IllegalArgumentException("reports can't be null or empty.");
 		}
-
 		for (final TestcaseReport report : reports) {
 			if (!report.validate()) {
 				if (ArtefactExtractor.logger.isDebugEnabled()) {
@@ -64,7 +53,6 @@ public class ArtefactExtractor extends AbstractMojo {
 	}
 
 	private void writeArtefacts() throws Exception {
-
 		for (final TestcaseReport report : reports) {
 			for (final StepReport stepreport : report.getSteps()) {
 				if (stepreport.getArtefact() == null) {
@@ -74,9 +62,7 @@ public class ArtefactExtractor extends AbstractMojo {
 					}
 					continue;
 				}
-
 				final File attachement = UtilsArtefact.saveAttachment(stepreport);
-
 				if (attachement == null) {
 					if (ArtefactExtractor.logger.isDebugEnabled()) {
 						ArtefactExtractor.logger
@@ -84,9 +70,10 @@ public class ArtefactExtractor extends AbstractMojo {
 					}
 					continue;
 				}
-
-				UtilsIO.copyFile(attachement, Paths.get(targetDirectory, report.getId(), stepreport.getDescription(),
-						UtilsDate.getCurrentTimeFormatted(new Date()), attachement.getName()).toFile());
+				UtilsIO.copyFile(attachement,
+						Paths.get(targetDirectory, report.getId(),
+								stepreport.getDescription().replace(":", "_").replace("\r\n", "_"),
+								UtilsDate.getCurrentTimestamp(), attachement.getName()).toFile());
 			}
 		}
 	}
@@ -96,9 +83,7 @@ public class ArtefactExtractor extends AbstractMojo {
 		try {
 			// init variables
 			init();
-
 			writeArtefacts();
-
 		} catch (final Exception ex) {
 			ArtefactExtractor.logger.error(ex);
 			throw new MojoExecutionException("can't create Testclass.");
