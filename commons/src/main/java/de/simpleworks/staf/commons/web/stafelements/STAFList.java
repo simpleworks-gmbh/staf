261365392c5ad3da70718f6e9a7b2e40cace06c1
mehmet-simsek <mehmet.simsek@simpleworks.de>
2022-02-24 23:31:38 +0100

    The missing import data added

--- commons/src/main/java/de/simpleworks/staf/commons/web/stafelements/STAFList.java
+++ commons/src/main/java/de/simpleworks/staf/commons/web/stafelements/STAFList.java
@@ -1,9 +1,14 @@
 package de.simpleworks.staf.commons.web.stafelements;
 
+import java.util.List;
+
+import de.simpleworks.staf.commons.exceptions.SystemException;
+import de.simpleworks.staf.commons.utils.Convert;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,7 +20,7 @@ public class STAFList extends STAFElement {
 
 	}
 
-	public List<WebElement> getAllElements() throws SystemException {
+	public List<WebElement> getAllElements() throw SystemException {
 		if (!exists()) {
 			throw new SystemException(String.format("No webElement can be found at '%s'.", getBy()));
 		}
