6f86b874567292911ce8d2f530536853b10ea3ef
dustin.baer <dustin.baer@simpleworks.de>
2022-10-05 21:23:42 +0200

    add implementation staf/issues/217

--- framework/src/main/java/de/simpleworks/staf/framework/api/httpclient/HttpClient.java
+++ framework/src/main/java/de/simpleworks/staf/framework/api/httpclient/HttpClient.java
@@ -16,6 +16,7 @@ import de.simpleworks.staf.commons.api.Header;
 import de.simpleworks.staf.commons.api.HttpRequest;
 import de.simpleworks.staf.commons.api.HttpResponse;
 import de.simpleworks.staf.commons.api.MultipartFormFileParameter;
+import de.simpleworks.staf.commons.api.RawFileParameter;
 import de.simpleworks.staf.commons.enums.ContentTypeEnum;
 import de.simpleworks.staf.commons.enums.HttpMethodEnum;
 import de.simpleworks.staf.commons.exceptions.SystemException;
@@ -197,12 +198,11 @@ public class HttpClient implements IHttpClient {
 			result = multipartBodyBuilder.build();
 			break;
 		default:
-			if (HttpClient.logger.isDebugEnabled()) {
-				HttpClient.logger
-						.debug(String.format("Content Type '%s' is not implemented yet.", contenttype.getValue()));
-				HttpClient.logger.debug("The Content Type was not defined, will return an empty request body.");
-			}
-			result = RequestBody.create(null, Convert.EMPTY_STRING);
+
+			final RawFileParameter rawFileParameter = request.getRawFileParameter();
+
+			result = RequestBody.create(MediaType.parse(contenttype.getValue()), new File(rawFileParameter.getFile()));
+
 		}
 		return result;
 	}
